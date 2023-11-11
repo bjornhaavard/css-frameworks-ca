ad4b49b3c508add9720bb0562b3404b30dd29f45
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-11 14:21:21 +0100

    trying to make a sort post function

--- src/js/handlers/sortPosts.js
+++ src/js/handlers/sortPosts.js
@@ -1,11 +1,16 @@
+// import { getPosts } from "../api/posts";
+
 const sortAction = document.querySelectorAll(".dropdown-menu li");
 
 console.log(sortAction);
 
-sortAction.forEach((link) => {
-  link.addEventListener("click", async () => {
-    const categoryId = link.dataset.id;
-    const posts = await getPosts(categoryId);
-    renderPosts(posts, "post-container");
+export async function sortPosts() {
+  sortAction.forEach((link) => {
+    link.addEventListener("click", async () => {
+      const categoryId = link.dataset.id;
+      const posts = await getPosts(categoryId);
+      renderPosts(posts, "post-container");
+      console.log(posts);
+    });
   });
-});
+}
