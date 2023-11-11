0bc0e43236f86fb84484fabf77c4bdc172d6e1e9
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-11 14:19:32 +0100

    removed log

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -2,5 +2,3 @@ export const API_HOST_URL = "https://api.noroff.dev";
 export const API_BASE = "/api/v1";
 export const API_SOCIAL_BASE = "/social";
 export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_BASE}`;
-
-console.log(API_SOCIAL_URL);
