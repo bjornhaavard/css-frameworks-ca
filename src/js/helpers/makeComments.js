cbb3d0bf18db747a219d99f19437b08c2d67330c
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-21 20:26:00 +0100

    made an avatar on the comments

--- src/js/helpers/makeComments.js
+++ src/js/helpers/makeComments.js
@@ -18,16 +18,20 @@ export async function getPostComments() {
       commentContainer.classList.add("rounded-4");
       commentContainer.classList.add("mt-5");
 
+      const avatar = document.createElement("img");
       const commentAuthor = document.createElement("h5");
       const commentPara = document.createElement("p");
-
+      const defaultAvatarImage = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg";
+      avatar.src = comment.author.avatar || defaultAvatarImage;
+      avatar.classList.add("img-fluid", "img-thumbnail", "rounded-circle", "post-thumbnail", "card");
+      avatar.style.width = "10%";
       commentPara.classList.add("fst-italic");
 
       commentSection.append(commentContainer);
-
+      console.log(comment.author.banner);
       commentAuthor.innerText = comment.author.name;
       commentPara.innerText = comment.body;
-      commentContainer.append(commentAuthor, commentPara);
+      commentContainer.append(commentAuthor, avatar, commentPara);
     });
   } else {
     const commentSection = document.querySelector("#commentSection");
