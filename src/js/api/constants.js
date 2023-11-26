a8e1cc72b5698fcbc917e07d13fdcc37886c1b96
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-26 17:19:44 +0100

    added image constants

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -2,3 +2,5 @@ export const API_HOST_URL = "https://api.noroff.dev";
 export const API_BASE = "/api/v1";
 export const API_SOCIAL_BASE = "/social";
 export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_BASE}`;
+export const defaultImage = "https://www.pacificfoodmachinery.com.au/media/catalog/product/placeholder/default/no-product-image-400x400_1.png";
+export const defaultAvatarImage = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg";
