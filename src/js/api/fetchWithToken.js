aa783aca5c4d13646b40086ada0d66c0f1478dc6
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-27 12:41:55 +0100

    displaying error on the screen

--- src/js/api/fetchWithToken.js
+++ src/js/api/fetchWithToken.js
@@ -1,10 +1,12 @@
+import { displayMessage } from "../components/shared/displayMessage.js";
 import { load } from "../storage/index.js";
 
 export function headers() {
   const token = load("token");
 
   if (!token) {
-    throw new Error("No token, pal");
+    // throw new Error("No token, pal");
+    displayMessage("#error-message", 'No token, go to <a href="/">Login page</a>', "danger");
   }
 
   return {
