b7d729f99f939d842dce90555eb0bbeb760a7bbb
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-11-29 20:51:42 +0100

    destructured the comment object

--- src/js/helpers/makeComments.js
+++ src/js/helpers/makeComments.js
@@ -9,6 +9,11 @@ export async function getPostComments() {
     const commentSection = document.querySelector("#commentSection");
 
     post.comments.forEach((comment) => {
+      const {
+        body,
+        author: { avatar, name },
+      } = comment;
+
       commentSection.classList.add("dflex");
       commentSection.classList.add("row");
       commentSection.classList.add("row-cols-lg-1");
@@ -18,20 +23,19 @@ export async function getPostComments() {
       commentContainer.classList.add("rounded-4");
       commentContainer.classList.add("mt-5");
 
-      const avatar = document.createElement("img");
+      const avatarPic = document.createElement("img");
       const commentAuthor = document.createElement("h5");
       const commentPara = document.createElement("p");
       const defaultAvatarImage = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg";
-      avatar.src = comment.author.avatar || defaultAvatarImage;
-      avatar.classList.add("img-fluid", "img-thumbnail", "rounded-circle", "post-thumbnail", "card");
-      avatar.style.width = "10%";
+      avatarPic.src = avatar || defaultAvatarImage;
+      avatarPic.classList.add("img-fluid", "img-thumbnail", "rounded-circle", "post-thumbnail", "card");
+      avatarPic.style.width = "10%";
       commentPara.classList.add("fst-italic");
 
       commentSection.append(commentContainer);
-      console.log(comment.author.banner);
-      commentAuthor.innerText = comment.author.name;
-      commentPara.innerText = comment.body;
-      commentContainer.append(commentAuthor, avatar, commentPara);
+      commentAuthor.innerText = name;
+      commentPara.innerText = body;
+      commentContainer.append(commentAuthor, avatarPic, commentPara);
     });
   } else {
     const commentSection = document.querySelector("#commentSection");
