0eaf57665c63e79a9742cd81f6d996c1e06f35e3
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-12-04 19:24:17 +0100

    added jsdocs and cleaned

--- src/js/handlers/logOut.js
+++ src/js/handlers/logOut.js
@@ -1,21 +1,25 @@
-import { remove } from "../storage";
+/**
+ * Logs the user out by removing stored authentication token and profile,
+ * and redirects the user to the home page.
+ *
+ * @example
+ * // Attach the logOut function to a logout button with the ID "logout-button"
+ * logOut();
+ */
+
+import { displayMessage } from "../components/shared/displayMessage.js";
+import * as storage from "../storage/index.js";
 
 export function logOut() {
   const logoutButton = document.querySelector("#logout-button");
   logoutButton.addEventListener("click", async (event) => {
     console.log(logoutButton);
     event.preventDefault();
-    try {
-      const result = await login(profile);
-      console.log(result);
-      const { accessToken, ...userProfile } = result;
 
-      storage.remove("token", accessToken);
-      storage.remove("profile", userProfile);
+    storage.remove("token");
+    storage.remove("profile");
 
-      location.href = "/";
-    } catch (error) {
-      displayMessage("#message", error.message, "warning");
-    }
+    location.href = "/";
+    displayMessage("#logout-message", "You are logged out", "success");
   });
 }
