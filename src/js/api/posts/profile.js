d911cf8514392f13da31da2484d389601fba4423
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-12-09 15:14:30 +0100

    jsdocs

--- src/js/api/posts/profile.js
+++ src/js/api/posts/profile.js
@@ -1,11 +1,21 @@
+import { displayMessage } from "../../components/shared/displayMessage.js";
 import { getName } from "../../helpers/getName.js";
 import { API_SOCIAL_URL } from "../constants.js";
 import { fetchWithToken } from "../fetchWithToken.js";
 
 const action = "/profiles";
 
+/**
+ * Gets the profile information for the currently authenticated user.
+ *
+ * @async
+ * @function
+ * @throws {Error} If there is an issue fetching the profile or the response is not OK.
+ * @returns {Promise<Object>} A Promise that resolves with the profile information.
+ */
+
 export async function getProfile() {
-  const profile = await getName();
+  const profile = getName();
 
   const response = await fetchWithToken(`${API_SOCIAL_URL}${action}/${profile}`);
 
@@ -13,5 +23,5 @@ export async function getProfile() {
     return await response.json();
   }
 
-  throw new Error(response.statusText);
+  throw new Error(displayMessage("#error-message", response.statusText, "warning"));
 }
--- src/js/helpers/getParamFromQueryString.js
+++ src/js/helpers/getParamFromQueryString.js
@@ -1,3 +1,12 @@
+/**
+ * Retrieves the value of a parameter from the query string of the current URL.
+ *
+ * @function
+ * @param {string} paramName - The name of the parameter to extract from the query string.
+ * @returns {string | undefined} The value of the specified parameter from the query string,
+ * or undefined if the parameter is not present in the query string.
+ */
+
 export function getParamFromQueryString(paramName) {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const paramValue = urlSearchParams.get(paramName);
